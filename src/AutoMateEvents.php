08fabf71dbe7fa72ee69f45378f241489f4ae4e5
Jugid <gidjulien@gmail.com>
2021-03-08 13:20:52 +0100

    Not a fix

--- src/AutoMateEvents.php
+++ src/AutoMateEvents.php
@@ -6,14 +6,10 @@ class AutoMateEvents
 {
     const RUNNER_SIMPLE_BEGIN = 'core:runner:simple:begin';
     const RUNNER_SIMPLE_END = 'core:runner:simple:end';
-
     const RUNNER_SPEC_BEGIN = 'core:runner:spec:begin';
     const RUNNER_SPEC_END = 'core:runner:spec:end';
-
     const RUNNER_ERROR = 'core:runner:error';
     const RUNNER_WIN = 'core:runner:win';
-
     const RUNNER_ENDS_ERROR = 'core:runner:end:error';
-
     const STEP_TRANSFORM = 'core:step:transform';
 }
