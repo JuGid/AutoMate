bcc9801879ab0fda5a67a7ea07570b9142c9e6d9
Jugid <gidjulien@gmail.com>
2021-03-23 00:05:11 +0100

    Example of using page + pagehandler implementation

--- src/Handler/PageHandler.php
+++ src/Handler/PageHandler.php
@@ -2,15 +2,40 @@
 
 namespace Automate\Handler;
 
+use Automate\Configuration\Configuration;
+use InvalidArgumentException;
+use Symfony\Component\Yaml\Yaml;
+
 abstract class PageHandler {
 
-    public $pageElements = [];
+    private static $pageElements = [];
 
-    public static function load(string $pageFile) {
+    private static string $pageName = '';
+    /**
+     * @param string $pageName Is the pageName in yaml format like wikipedia.index 
+     * where wikipedia is the folder and index the file
+     */
+    public static function load(string $pageName) {
+        self::$pageName = $pageName;
+
+        $pagesPath = Configuration::get('pages.folder');
         
+        $pageNameSplit = explode(".", $pageName);
+
+        foreach($pageNameSplit as $path) {
+            $pagesPath .= ('/' . $path);
+        }
+
+        self::$pageElements = Yaml::parse(file_get_contents($pagesPath . '.yaml'));
     }
 
     public static function get(string $element) {
+        $elements = array_values(self::$pageElements)[0];
 
+        if(!isset($elements[$element])) {
+            throw new InvalidArgumentException('Element '.$element.' does not exist in page ' . self::$pageName);
+        }
+        
+        return $elements[$element];
     }
 }
