7fcefb59eb751cab91bc437e8460aea5e2d79c9a
Jugid <gidjulien@gmail.com>
2021-03-22 23:26:57 +0100

    Example with page

---
+++ src/Handler/PageHandler.php
@@ -0,0 +1,16 @@
+<?php 
+
+namespace Automate\Handler;
+
+abstract class PageHandler {
+
+    public $pageElements = [];
+
+    public static function load(string $pageFile) {
+
+    }
+
+    public static function get(string $element) {
+
+    }
+}
